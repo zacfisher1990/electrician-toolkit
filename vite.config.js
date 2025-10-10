1f7b0fdcb222f635a9142ebb579021d6388b5c8f
zacfisher1990 <zcfshr@gmail.com>
2025-10-09 22:56:34 -0700

    Add PWA support with icons and manifest

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/electrician-toolkit/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        sw: 'public/service-worker.js'
+      }
+    }
+  }
 })
