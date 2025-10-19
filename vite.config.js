559e10fec923458e1c0f378cd09365237f5ef343
zacfisher1990 <zcfshr@gmail.com>
2025-10-19 02:06:07 -0700

    changing vite.config.js so it'll work on vercel

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => {
-  const base = command === 'serve' ? '/' : '/electrician-toolkit/';
-  
-  return {
-    plugins: [react()],
-    base: base,
-    build: {
-      outDir: 'dist'
-    }
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist'
   }
 })
